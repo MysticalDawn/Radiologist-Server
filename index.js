c50260638cffc3599e30857dc18900084e50642a
Abdulwahab <wahab210899@gmail.com>
2025-05-04 05:16:32 +0300

    jhejh

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin : ""}));
 app.use(cors({ origin: "https://radiology-website-zeta.vercel.app" }));
 app.use("/", upload_bucket);
 app.use("/", populateHandler);
